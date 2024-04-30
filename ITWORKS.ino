8a7485359463769f4b332dec78cce98f4df50ccd
bpain <145798509+bpain@users.noreply.github.com>
2024-04-29 19:07:40 -0700

    Complete? pre-ROS GPS interface

--- ITWORKS.ino
+++ ITWORKS.ino
@@ -14,20 +14,28 @@ void setup() {
  }
 
 void loop() {
-  while(serial_connection.available()){
+  read_inputs(); 
+  delay(4000); 
+  // put your main code here, to run repeatedly:
+}
+
+void read_inputs(){
+   while(serial_connection.available()){
    if(gps.encode(serial_connection.read())){
      String msg = serial_connection.readStringUntil('\r');
      Serial.println(msg);
      Serial.println("LAT=");Serial.println(gps.location.lat(),6);
      Serial.println("LONG=");Serial.println(gps.location.lng(),6);
-     delay(4000);
-
+     Serial.println("ALT=");Serial.println(gps.altitude.meters(),6);
    }
 
   }
-  
-  
-  // put your main code here, to run repeatedly:
 }
-
-// put function definitions here:eturn x + y;
+// put function definitions here:return x + y;
+double read_altitude(){
+ while(serial_connection.available()){
+  if(gps.encode(serial_connection.read())){
+    return gps.altitude.meters(); 
+  }
+ } 
+}
